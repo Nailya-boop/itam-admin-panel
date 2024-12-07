61904fd5290ce1ac74f1dac1a27268645426843e
Valet-V0ult-de-Fur1e <82310399+Valet-V0ult-de-Fur1e@users.noreply.github.com>
2024-12-07 23:21:46 +0300

    Update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -4,7 +4,7 @@ import { MainPage } from './pages/MainPage';
  import { RegisterPage } from './pages/RegisterPage';
 import { ChangePassPage } from './pages/ChangePassPage';
 // import { LoginPage } from './pages/LoginPage';
-import { BrowserRouter, Routes, Route } from 'react-router-dom';
+import { BrowserRouter, Routes, Route, Navigate } from 'react-router-dom';
 
 const App = () => {
   return (
@@ -14,6 +14,7 @@ const App = () => {
         <Route path='/singin' element={<RegisterPage/>}/>
         <Route path='/changepass' element={<ChangePassPage/>}/>
         <Route path='/main' element={<MainPage/>}/>
+        <Route path="/" element={<Navigate to="/main" />} />
       </Routes>
     </BrowserRouter>
   );
